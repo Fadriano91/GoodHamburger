1947794d7d923a3ba30d04274f08d02bf8d8ba9a
Francis <fquimico@hotmail.com.br>
2024-01-26 11:31:57 -0300

    adjust-mapping

--- GoodHamburger/Data/Map/OrderMap.cs
+++ GoodHamburger/Data/Map/OrderMap.cs
@@ -10,30 +10,11 @@ namespace GoodHamburger.Data.Map
         public void Configure(EntityTypeBuilder<OrderModel> builder)
         {
 
-
-
-
-            //builder.HasMany(ex => ex.Extras)
-            //    .WithMany(e => e.Orders)
-            //    .UsingEntity<OrderExtraModel>(j => j.ToTable("OrderExtras"));
-
             builder.HasMany(x => x.Extras)
             .WithMany(p => p.Orders)
             .UsingEntity<OrderExtraModel>(
             p => p.HasOne(pa => pa.Extra).WithMany(),
             p => p.HasOne(pa => pa.Order).WithMany().HasForeignKey(pa => pa.OrderId));
-
-
-            // Indica que é uma relação muitos-para-muitos
-            //.UsingEntity<Dictionary<string, object>>(
-            //    "OrderExtra",
-            //    j => j.HasOne<ExtraModel>().WithMany().HasForeignKey("ExtraId"),
-            //    j => j.HasOne<OrderModel>().WithMany().HasForeignKey("OrderId"),
-            //    j =>
-            //    {
-            //        j.HasKey("OrderId", "ExtraId");
-            //    }
-
         }
     }
 }
